5a3af0c0c4dc8bd6317f0b94ab46c5afb6c422b9
Becky <bckyjns@gmail.com>
2025-12-04 20:29:43 +0000

    day 1 - part 2

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -9,17 +9,41 @@ defmodule AdventOfCode.Day01 do
     end)
   end
 
-  defp perform_rotation({:L, turns}, value), do: rem(value - turns + 100, 100)
-  defp perform_rotation({:R, turns}, value), do: rem(value + turns, 100)
+  defp perform_rotation({:R, turns}, value) do
+    new_value = rem(value + turns, 100)
+    wraps = count_wraps(:R, value, turns)
+    {new_value, wraps}
+  end
+
+  defp perform_rotation({:L, turns}, value) do
+    new_value = rem(rem(value - turns, 100) + 100, 100)
+    wraps = count_wraps(:L, value, turns)
+    {new_value, wraps}
+  end
+
+  defp count_wraps(:R, value, turns), do: div(value + turns, 100)
+  defp count_wraps(:L, 0, turns), do: div(turns, 100)
+  defp count_wraps(:L, value, turns) when turns >= value, do: 1 + div(turns - value, 100)
+  defp count_wraps(:L, _value, _turns), do: 0
 
   @spec solve(String.t()) :: integer()
   def solve(file_name) do
     parse_input(file_name)
     |> Enum.reduce({50, 0}, fn rotation, {current, count} ->
       case perform_rotation(rotation, current) do
-        0 -> {0, count + 1}
-        new_value -> {new_value, count}
+        {0, _wraps} -> {0, count + 1}
+        {new_value, _wraps} -> {new_value, count}
       end
+    end)
+    |> elem(1)
+  end
+
+  @spec actually_solve(String.t()) :: integer()
+  def actually_solve(file_name) do
+    parse_input(file_name)
+    |> Enum.reduce({50, 0}, fn rotation, {current, count} ->
+      {new_value, wraps} = perform_rotation(rotation, current)
+      {new_value, count + wraps}
     end)
     |> elem(1)
   end
--- test/advent_of_code_day_01_test.exs
+++ test/advent_of_code_day_01_test.exs
@@ -5,4 +5,8 @@ defmodule AdventOfCodeDay01Test do
   test "solves example" do
     assert AdventOfCode.Day01.solve("inputs/day_01_example.txt") == 3
   end
+
+  test "actually solves example" do
+    assert AdventOfCode.Day01.actually_solve("inputs/day_01_example.txt") == 6
+  end
 end
