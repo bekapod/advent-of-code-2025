c7f4be1e50b2b4213eb68a548196af04682c786c
Becky <bckyjns@gmail.com>
2025-12-10 21:04:14 +0000

    day 2 - part 2

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -21,20 +21,39 @@ defmodule AdventOfCode.Day02 do
       |> then(fn {first, second} -> first == second end)
   end
 
-  defp get_invalid_ids(range) do
-    Enum.filter(range, &invalid_id?/1)
+  defp actually_invalid_id?(id) when id < 10, do: false
+
+  defp actually_invalid_id?(id) do
+    str_id = Integer.to_string(id)
+    len = String.length(str_id)
+
+    1..div(len, 2)
+    |> divisors_of(len)
+    |> Enum.any?(&is_repeated_pattern?(str_id, len, &1))
+  end
+
+  defp divisors_of(range, len) do
+    Enum.filter(range, &(rem(len, &1) == 0))
+  end
+
+  defp is_repeated_pattern?(str_id, len, pattern_len) do
+    pattern = String.slice(str_id, 0, pattern_len)
+    String.duplicate(pattern, div(len, pattern_len)) == str_id
   end
 
   @spec solve(String.t()) :: integer()
   def solve(file_name) do
     file_name
     |> parse_input()
-    |> Enum.flat_map(&get_invalid_ids/1)
+    |> Enum.flat_map(fn range -> Enum.filter(range, &invalid_id?/1) end)
     |> Enum.sum()
   end
 
   @spec actually_solve(String.t()) :: integer()
   def actually_solve(file_name) do
-    parse_input(file_name)
+    file_name
+    |> parse_input()
+    |> Enum.flat_map(fn range -> Enum.filter(range, &actually_invalid_id?/1) end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code_day_02_test.exs
+++ test/advent_of_code_day_02_test.exs
@@ -5,4 +5,8 @@ defmodule AdventOfCodeDay02Test do
   test "solves example" do
     assert AdventOfCode.Day02.solve("inputs/day_02_example.txt") == 1_227_775_554
   end
+
+  test "actually solves example" do
+    assert AdventOfCode.Day02.actually_solve("inputs/day_02_example.txt") == 4_174_379_265
+  end
 end
